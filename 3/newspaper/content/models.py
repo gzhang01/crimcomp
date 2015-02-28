08c844c8e7e265df7c738e4848cb30412c4b7f15
George Zhang <George@Georges-MacBook-Pro-3.local>
2015-02-28 18:18:17 -0500

    finished assignment 3

---
+++ 3/newspaper/content/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Content(models.Model):
+	title = models.CharField(max_length = 500)
+	subtitle = models.CharField(max_length = 500)
+	contributors = models.ManyToManyField('Contributor', null = True, related_name = 'content')
+	pub_date = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.title
+
+class Article(Content):
+	text = models.CharField(max_length = 1000)
+
+class Image(Content):
+	path = models.FilePathField(path = '/home/Dropbox/CrimComp/Assignments/3')
+
+	# not exactly sure what save is supposed to be doing...
+	def save(self, *args, **kwargs):
+		return
+
+	def info(self):
+		return '{0}\n{1}\npath: {2}'.format(self.title, self.subtitle, self.path)
+
+class Contributor(models.Model):
+	first_name = models.CharField(max_length = 500)
+	last_name = models.CharField(max_length = 500)
+
+	def __str__(self):
+		return '{0}, {1}'.format(self.last_name, self.first_name)
+
+	def die(self):
+		for c in self.contents:
+			c.contributors.remove(self)
+		self.delete()
+
